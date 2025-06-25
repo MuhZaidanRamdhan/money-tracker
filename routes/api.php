7be26119f752f2fe2a40fc241cfa5e862fca975f
MuhZaidanRamdhan <muhammadzaidanramdhan509@gmail.com>
2025-06-25 20:24:10 +0700

    vercel

--- routes/api.php
+++ routes/api.php
@@ -21,21 +21,21 @@ Route::post('/password/reset', [AuthController::class, 'resetPassword']);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
-  
+
   Route::post('/logout', [AuthController::class, 'logout']);
   Route::post('/password/change-password', [AuthController::class, 'changePassword']);
 });
 
+Route::get('/categories', [CategoriesController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-
+  
   Route::get('/users', [UserController::class, 'index']);
   Route::get('/users/{id}', [UserController::class, 'show']);
   Route::post('/users', [UserController::class, 'store']);
   Route::put('/users/{id}', [UserController::class, 'update']);
   Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-  Route::get('/categories', [CategoriesController::class, 'index']);
+  
   Route::get('/categories/{id}', [CategoriesController::class, 'show']);
   Route::post('/categories', [CategoriesController::class, 'store']);
   Route::put('/categories/{id}', [CategoriesController::class, 'update']);
